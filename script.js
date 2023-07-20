c6ce022ef6810ea84494ba427d85854d7a1f5b21
Larissa Cerqueira <109742068+LaYotsuna@users.noreply.github.com>
2023-07-20 15:28:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,52 +10,69 @@ const container = document.querySelector(".container");
 let content = [];
 let counter = 0;
 
-function addNewTask() {
+/* function addNewTask() {
   // Make the textarea appear or disappear
   newTaskBtn.addEventListener("click", () => {
     taskInput.classList.toggle("hidden");
     taskInput.focus();
     // Reset the textarea
     taskInput.value = "";
-  });
+  }); */
 
-  // Function to insert new task into the task text area
-  function pushNewTask(txt) {
-    content.push({
-      text: txt,
-      done: false,
-    }); // Add the new task to the content array
-    listBox.insertAdjacentHTML(
-      "beforeend",
-      `
+function addNewTask() {
+  newTaskBtn.addEventListener("click", () => {
+    const text = taskInput.value.trim();
+
+    if (window.innerWidth <= 950) {
+      taskInput.classList.toggle("hidden");
+      if (text !== "") {
+        pushNewTask(text);
+      }
+    } else {
+      taskInput.classList.toggle("hidden");
+      taskInput.focus();
+      // Reset the textarea
+      taskInput.value = "";
+    }
+  });
+}
+
+// Function to insert new task into the task text area
+function pushNewTask(txt) {
+  content.push({
+    text: txt,
+    done: false,
+  }); // Add the new task to the content array
+  listBox.insertAdjacentHTML(
+    "beforeend",
+    `
       <div class="list-item">
         <i class="gg-check-r"></i>
         <span class="item">${txt}</span>
         <i class="gg-close"></i>
       </div>
     `
-    );
-    taskInput.value = "";
-    saveTasks(); // Call saveTasks() to update the saved tasks in localStorage
+  );
+  taskInput.value = "";
+  saveTasks(); // Call saveTasks() to update the saved tasks in localStorage
+}
+
+taskInput.addEventListener("keyup", (event) => {
+  // Add the written task to the to-do list after pressing enter
+  if (event.key === "Enter") {
+    const text = taskInput.value.trim();
+
+    if (text !== "") {
+      pushNewTask(text);
+    }
   }
 
-  taskInput.addEventListener("keyup", (event) => {
-    // Add the written task to the to-do list after pressing enter
-    if (event.key === "Enter") {
-      const text = taskInput.value.trim();
-
-      if (text !== "") {
-        pushNewTask(text);
-      }
-    }
-
-    if (event.key === "Escape") {
-      taskInput.value = "";
-      taskInput.classList.add("hidden");
-      location.reload(); // Fix bug where done tasks would not get styled unless the page was reloaded, so after you exit the textarea the page reloads
-    }
-  });
-}
+  if (event.key === "Escape") {
+    taskInput.value = "";
+    taskInput.classList.add("hidden");
+    location.reload(); // Fix bug where done tasks would not get styled unless the page was reloaded, so after you exit the textarea the page reloads
+  }
+});
 
 // Setter
 function saveTasks() {
